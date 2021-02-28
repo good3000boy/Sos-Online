80da3c3186bf342d69c3a9f84e2c3f7f0c71c94e
good3000boy <69675900+good3000boy@users.noreply.github.com>
2021-03-01 02:19:22 +0330

    Add files via upload

---
+++ Online/urls.py
@@ -0,0 +1,31 @@
+"""SOS URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('accounts.urls'), name='accounts'),
+    path('', include('building.urls'), name='building'),
+    path('', include('education.urls'), name='education'),
+    path('', include('insurance.urls'), name='insurance'),
+    path('', include('internet.urls'), name='internet'),
+    path('', include('repair.urls'), name='repair'),
+    path('', include('software.urls'), name='software'),
+    path('', include('shad.urls'), name='shad'),
+    path('', include('service.urls'), name='service'),
+
+]
